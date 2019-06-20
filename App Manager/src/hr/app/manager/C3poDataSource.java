13346bac19ffa69a536e31d7c18465abc483782f
tukic <tu50407@fer.hr>
2019-06-20 13:39:06 +0200

    created test DB

--- App Manager/src/hr/app/manager/C3poDataSource.java
+++ App Manager/src/hr/app/manager/C3poDataSource.java
@@ -12,10 +12,42 @@ public class C3poDataSource {
 	 
     static {
         try {
+        	
+        	/*
+        	 * 			!!! TEST !!!
+        	 * 
+        	 *         	Database: test_app_ukic
+        	 *			Username: app_test_johto
+        	 *			Email: b1190020@urhen.com
+        	 *			Password: antejoni
+        	 *
+
+        	
+            cpds.setDriverClass("com.mysql.jdbc.Driver");
+            cpds.setJdbcUrl("jdbc:mysql://85.10.205.173:3306/test_app_ukic");
+            cpds.setUser("app_test_johto");
+            cpds.setPassword("antejoni");
+            
+            /
+        	
+        	/* 
+        	 * 			!!! REAL !!!
+        	 * 
+        	 * 			Database: app_ukic
+        	 * 			Username: johto_db
+        	 * 			Email: tomislav.ukic7@gmail.com 
+        	 * 			db4free.net
+        	 * 			ip: 85.10.205.173
+        	 *			port: 3306
+        	 */
+        	
+        
             cpds.setDriverClass("com.mysql.jdbc.Driver");
             cpds.setJdbcUrl("jdbc:mysql://85.10.205.173:3306/app_ukic");
             cpds.setUser("johto_db");
             cpds.setPassword("antejoni2007");
+            
+            
         } catch (PropertyVetoException e) {
             // handle the exception
         }
